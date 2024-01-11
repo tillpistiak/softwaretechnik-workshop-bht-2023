c64e18029677cdfb2cc54e2425a609d5f64caaf5
Till Pistiak <till.pistiak@gmail.com>
2024-01-11 17:49:27 +0100

    added entities

--- src/main/java/de/bht/azur/model/Group.java
+++ src/main/java/de/bht/azur/model/Group.java
@@ -1,9 +1,9 @@
-package de.bht.azur;
+package de.bht.azur.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 
 @Entity
-public class TestEntity extends PanacheEntity {
-    public String name;
+public class Group extends PanacheEntity {
+
 }
---
+++ src/main/java/de/bht/azur/model/User.java
@@ -0,0 +1,9 @@
+package de.bht.azur.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class User extends PanacheEntity {
+
+}
