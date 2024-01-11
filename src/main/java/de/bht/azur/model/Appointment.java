f1d3a9a0dca4eadcb83164e90baac8423b2659b0
Till Pistiak <till.pistiak@gmail.com>
2024-01-11 18:00:41 +0100

    added attributes and lombok dependency

--- src/main/java/de/bht/azur/model/Appointment.java
+++ src/main/java/de/bht/azur/model/Appointment.java
@@ -1,9 +1,32 @@
 package de.bht.azur.model;
 
+import java.time.LocalDateTime;
+
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class Appointment extends PanacheEntity {
+    @Id
+    @GeneratedValue
+    @Getter
+    @Setter
+    private Long id;
+    @Getter
+    @Setter
+    private String description;
+    @Getter
+    @Setter
+    private String title;
+    @Getter
+    @Setter
+    private LocalDateTime start;
+    @Getter
+    @Setter
+    private LocalDateTime end;
 
 }
--- src/main/java/de/bht/azur/model/Group.java
+++ src/main/java/de/bht/azur/model/Group.java
@@ -2,8 +2,17 @@ package de.bht.azur.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class Group extends PanacheEntity {
+    @Id
+    @GeneratedValue
+    @Getter
+    @Setter
+    private Long id;
 
 }
